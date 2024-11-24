c5027778c3573a1141d1ebac024b426f318b139d
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-24 19:50:03 +0530

    Operations on set

--- Sets.py
+++ Sets.py
@@ -12,9 +12,9 @@ print(Set)
 
 
 s1 = {1 ,5 ,6 ,7, 4, 9}
-s2 = {1 ,2 ,6 ,3, 4, 8}
+s3 = {1 ,2 ,6 ,3, 4, 8}
 
 print("Difference" , s1.difference(s2))
 print("Symetric" , s1.symmetric_difference(s2))
-print(s1.intersection(s2))
-print(s1.union(s2))
+print(s1.intersection(s3))
+print(s1.union(s3))
