a5e167662abec21c04c1d07c959c3a7148ecafba
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-24 19:39:33 +0530

    Operations On Sets

---
+++ Sets.py
@@ -0,0 +1,16 @@
+Se = {}
+print(Se)
+
+Set = {1 ,22 ,333 ,4444 , 55555 , 666666}
+print(Set)
+
+Set.add(8)
+print(Set)
+
+
+
+s1 = {1 ,5 ,6 ,7, 4, 9}
+s2 = {1 ,2 ,6 ,3, 4, 8}
+
+print("Difference" , s1.difference(s2))
+print("Symetric" , s1.symmetric_difference(s2))
