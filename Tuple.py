e0f18e6d9f9ba5651ed2cb3f8584a5da528d0c93
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-24 19:25:36 +0530

    Operations on Tuples

---
+++ Tuple.py
@@ -0,0 +1,10 @@
+tuple = ()
+print(tuple)
+
+tuple = ("hanzla" , (18 , 7 ,45) , 23 , 445 , 118)
+print(tuple)
+
+tuple = ("hanzla" , (18 , 7 , 45 ) , ["Hassan" , "Abudar"] , 23 , 445 , 118)
+print(tuple)
+
+print(tuple[2][1])
